72dc71ca5cd571b6e6a0729c2055f888c1a1c883
Erin Furuichi <erinf@Erins-MacBook-Pro.local>
2018-04-22 16:59:31 -1000

    3 zoom

--- js/app.js
+++ js/app.js
@@ -29,6 +29,13 @@ function lessContent(){
 //3. Tacocat, The Original Palindrome King
 /*Create a function named `zoom` that will increase the font size of the paragraph with the id of `biggie` after hovering your mouse over it. Increae the font size to 150%*/
 
+function zoom(){
+    if (biggie.style.fontSize === "100%"){
+        biggie.style.fontSize = "100%";
+    }else{
+        biggie.style.fontSize = "150%";
+    }
+}
 
 //4. McDonalds
 /*Create a variable name menuItems and assign it an array of three of your favorite items at McDonald's.
